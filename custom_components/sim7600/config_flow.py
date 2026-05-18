95e8782429504f12d74829587a8bf83d1fe118af
Timo Schoormann <timo.schoormann@users.noreply.github.com>
2026-05-18 11:43:57 +0200

    feat(config): allow GNSS polling interval down to 15s

--- custom_components/sim7600/config_flow.py
+++ custom_components/sim7600/config_flow.py
@@ -71,7 +71,7 @@ class Sim7600ConfigFlow(ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
                     ): vol.All(vol.Coerce(int), vol.Range(min=15)),
                     vol.Optional(
                         CONF_GNSS_INTERVAL, default=DEFAULT_GNSS_INTERVAL
-                    ): vol.All(vol.Coerce(int), vol.Range(min=60)),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=15)),
                     vol.Optional(CONF_DEBUG_MODE, default=False): bool,
                 }
             ),
